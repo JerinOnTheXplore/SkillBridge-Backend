bb19eda885d5b5d604f27d0ebb58de88c2995de0
JerinXplore <jerinjerin101325@gmail.com>
2026-01-28 10:19:23 +0600

    feat(error): added global error handling middleware

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application } from "express";
 import { authRoutes } from "./modules/auth/auth.route";
+import { globalErrorHandler } from "./middlewares/errorHandler";
 
 
 const app:Application = express();
@@ -7,7 +8,9 @@ app.use(express.json());
 
 app.get("/",(req,res)=>{
     res.send("Hello, Server !!")
-})
+});
+app.use(globalErrorHandler);
+
 app.use("/api/auth",authRoutes);
 
 export default app;
